82fdc7fb5c54b835c96019426c0bb14f8407a7ad
Cevu01 <vuk.v.stankovic01@gmail.com>
2025-03-08 17:58:25 +0100

    Configuring nativewind

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,14 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl text-blue-600">Welcome Vuk</Text>
+      <Link href="/onboarding">OnBoarding</Link>
+      <Link href={{ pathname: "/movies/[id]", params: { id: "avangers" } }}>
+        Avengers Movie
+      </Link>
     </View>
   );
 }
